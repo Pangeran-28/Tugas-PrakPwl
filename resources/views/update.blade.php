6c919e96eef431df25efe5388efe8d70ce3c32dd
Pangeran-28 <pangeranebenezer@gmail.com>
2024-05-02 02:32:10 +0700

    Tugas PrakPwl

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,31 +12,31 @@
 <body>
     <div class="container">
         <h2 class="mt-4">Form Update Data Mahasiswa</h2>
-        <form action="{{route('update', $mahasiswa->id_mahasiswa)}}" method="POST">
+        <form action="{{ route('update', $mahasiswa->id_mahasiswa) }}" method="post">
             @csrf
             @method('PUT')
-                    <div class="form-group">
-                        <label for="nama">Nama:</label>
-                        <input type="text" class="form-control" id="nama" name="nama" value="{{$mahasiswa->nama}}" required>
-                    </div>
-        
-                    <div class="form-group">
-                        <label for="nim">NIM:</label>
-                        <input type="text" class="form-control" id="nim" name="nim" value="{{$mahasiswa->nim}}" required>
-                    </div>
-        
-                    <div class="form-group">
-                        <label for="jurusan">Jurusan:</label>
-                        <input type="text" class="form-control" id="jurusan" name="jurusan" value="{{$mahasiswa->jurusan}}" required>
-                    </div>
-        
-                    <div class="form-group">
-                        <label for="alamat">Alamat:</label>
-                        <input type="text" class="form-control" id="alamat" name="alamat" value="{{$mahasiswa->alamat}}" required>
-                    </div>
-        
-                    <button type="submit" class="btn btn-primary mt-3">Update</button>
-                </form>
+            <div class="form-group">
+                <label for="nama">Nama:</label>
+                <input type="text" name="nama" class="form-control"  id="nama"  value="{{ $mahasiswa->nama }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="nim">NIM:</label>
+                <input type="text" name="nim" class="form-control" id="nim" value="{{ $mahasiswa->nim }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="jurusan">Jurusan:</label>
+                <input type="text" name="jurusan" class="form-control" id="jurusan" value="{{ $mahasiswa->jurusan }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="alamat">Alamat:</label>
+                <input type="text" name="alamat" class="form-control" id="alamat"  value="{{ $mahasiswa->alamat }}" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-3">Update</button>
+        </form>
     </div>
 
     <!-- Bootstrap JS (optional) -->
